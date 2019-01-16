fc84754a93a945fd026fa7cc3e329f06474d16fd
Andrei Sakharuk <andreysakharuk@gmail.com>
2019-01-16 13:18:59 +0300

    Setup for running Selenium Test tesng gradle task was updated for running SeleniumScenarioTest

---
+++ src/test/java/com/epam/cdp/selenium/SeleniumTest.java
@@ -0,0 +1,18 @@
+package com.epam.cdp.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void testConsumer(){
+        System.setProperty("webdriver.chrome.driver", "libs/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.consumerreports.org/cro/index.htm");
+        driver.quit();
+        Assert.assertTrue(true);
+    }
+}
